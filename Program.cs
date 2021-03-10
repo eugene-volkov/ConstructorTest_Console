e57ff51aacfe89663627aded4d626837cadc270d
Eugene Volkov <bizzar@list.ru>
2021-03-10 23:09:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstructorTest_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ConstructorTest();
+        }
+    }
+}
